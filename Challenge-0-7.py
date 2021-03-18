7015ba5ea79d264c46dfe1dce8ecae77ff89c421
Vuyisa Mrwebi <73329804+Flex-FLEZZY@users.noreply.github.com>
2021-03-18 10:33:59 +0200

    Added task 07

---
+++ Challenge-0-7.py
@@ -0,0 +1,11 @@
+def conv_celsius(celsius):
+    deg_celsius = (celsius * 1.8) + 32
+    return deg_celsius
+
+print(conv_celsius(celsius))
+
+def conv_fahrenheit(fahrenheit):
+    dag_fahrenheit = (fahrenheit - 32) * 5 / 9
+    return dag_fahrenheit
+
+print(conv_fahrenheit(fahrenheit))
